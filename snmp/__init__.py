d1949d78128ddf69cc9db7c69400732b6b77034a
Charles Tolley <charlestolley@gmail.com>
2019-04-02 10:35:56 -0600

    Moved _listener function inside of Manager class

--- snmp/__init__.py
+++ snmp/__init__.py
@@ -6,7 +6,7 @@ import socket
 import threading
 import time
 
-from .exceptions import ProtocolError, Timeout, STATUS_ERRORS
+from .exceptions import EncodingError, ProtocolError, Timeout, STATUS_ERRORS
 from .mutex import RWLock
 from .types import *
 
@@ -16,33 +16,8 @@ PORT = 161
 RECV_SIZE = 65507
 MAX_REQUEST_ID = 0xffffffff
 
-def _listener(sock, data, lock, signal, done):
-    while True:
-        r, _, _ = select.select([sock, done], [], [])
-        if done in r:
-            return
-
-        packet, (host, port) = sock.recvfrom(RECV_SIZE)
-        if port != PORT:
-            continue
-
-        try:
-            message = ASN1.deserialize(packet, cls=Message)
-
-            if message.version != 0:
-                continue
-
-            message.poke()
-
-        except (EncodingError, ProtocolError) as e:
-            continue
-
-        with lock:
-            data[message.data.request_id.value] = message
-
-        signal.set()
-
 class Manager:
+    VERSION = 0
     def __init__(self, community=None, rwcommunity=None):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._sock.setblocking(False)
@@ -59,15 +34,38 @@ class Manager:
         self._count_by = random.randint(0, MAX_REQUEST_ID//2) * 2 + 1
         self._next_id = self._count_by
 
-        self._responses = {}
-        self._lock, wlock = RWLock()
+        self._data = {}
+        self._data['rlock'], self._data['wlock'] = RWLock()
         self._received = threading.Event()
 
-        self._listener = threading.Thread(
-            target=_listener,
-            args=(self._sock, self._responses, wlock, self._received, self._read_pipe),
-        )
+        self._listener = threading.Thread(target=self._listen)
         self._listener.start()
+
+    def _listen(self):
+        while True:
+            r, _, _ = select.select([self._sock, self._read_pipe], [], [])
+            if self._read_pipe in r:
+                break
+
+            packet, (host, port) = self._sock.recvfrom(RECV_SIZE)
+            if port != PORT:
+                continue
+
+            try:
+                message = ASN1.deserialize(packet, cls=Message)
+
+                if message.version != self.VERSION:
+                    continue
+
+                message.poke()
+
+            except (EncodingError, ProtocolError) as e:
+                continue
+
+            with self._data['wlock']:
+                self._data[message.data.request_id.value] = message
+
+            self._received.set()
 
     def close(self):
         log.debug("Sending shutdown signal to listener thread")
@@ -134,10 +132,10 @@ class Manager:
             kwargs['community'] = community
 
         request_id = pdu.request_id.value
-        message = Message(data=pdu, **kwargs)
+        message = Message(version=self.VERSION, data=pdu, **kwargs)
 
-        with self._lock:
-            self._responses[request_id] = None
+        with self._data['rlock']:
+            self._data[request_id] = None
 
         packet = message.serialize()
 
@@ -148,8 +146,8 @@ class Manager:
             while self._received.wait(end_time - time.time()):
                 self._received.clear()
 
-                with self._lock:
-                    response = self._responses[request_id]
+                with self._data['rlock']:
+                    response = self._data[request_id]
 
                 if response is not None:
                     pdu = response.data
