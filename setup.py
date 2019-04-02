29430e83b8112d89c7abe3062d1e9d9b6b567252
Charles Tolley <charlestolley@gmail.com>
2019-04-02 10:44:12 -0600

    Publishing as version 0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(directory + os.path.sep + "README.md", "r") as fh:
 
 setuptools.setup(
     name="snmp",
-    version="0.0.5",
+    version="0.0.6",
     author="Charles Tolley",
     author_email="charlestolley@gmail.com",
     license="GPLv3+",
