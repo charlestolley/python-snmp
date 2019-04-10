6366d0dbfa379db90375ddd71671736275c9dabf
Chuck Tolley <charles_tolley@byu.edu>
2019-04-10 15:27:36 -0600

    Created Manager factory function

--- snmp/__init__.py
+++ snmp/__init__.py
@@ -1,1 +1,14 @@
-from .v1 import SNMPv1 as Manager
+from .v1 import SNMPv1
+
+versions = {
+    1: SNMPv1,
+}
+
+def Manager(*args, version=1, **kwargs):
+    try:
+        cls = versions[version]
+    except KeyError as e:
+        msg = "'version' must be one of {}".format(list(versions.keys()))
+        raise ValueError(msg) from e
+
+    return cls(*args, **kwargs)
