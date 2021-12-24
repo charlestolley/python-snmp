844326b3b025f95dc28e6942a84c725c41b2251a
Charles Tolley <charlestolley@gmail.com>
2021-12-24 11:52:39 -0800

    Implement key features of SMIv2

---
+++ snmp/smi/v2.py
@@ -0,0 +1,54 @@
+__all__ = [
+    "Integer32", "IpAddress", "Counter32", "Gauge32",
+    "Unsigned32", "TimeTicks", "Opaque", "Counter64",
+    "zeroDotZero",
+]
+
+from socket import inet_aton, inet_ntoa
+from snmp.ber import *
+from snmp.types import *
+
+class Unsigned(Integer):
+    SIGNED = False
+
+class Integer32(Integer):
+    pass
+
+class IpAddress(OctetString):
+    TYPE = Identifier(CLASS_APPLICATION, STRUCTURE_PRIMITIVE, 0)
+    MIN_SIZE = 4
+    MAX_SIZE = 4
+
+    def __init__(self, addr):
+        self.addr = addr
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, repr(self.addr))
+
+    @property
+    def data(self):
+        return inet_aton(self.addr)
+
+    @classmethod
+    def parse(cls, data):
+        return cls(inet_ntoa(data))
+
+class Counter32(Unsigned):
+    TYPE = Identifier(CLASS_APPLICATION, STRUCTURE_PRIMITIVE, 1)
+
+class Gauge32(Unsigned):
+    TYPE = Identifier(CLASS_APPLICATION, STRUCTURE_PRIMITIVE, 2)
+
+Unsigned32 = Gauge32
+
+class TimeTicks(Unsigned):
+    TYPE = Identifier(CLASS_APPLICATION, STRUCTURE_PRIMITIVE, 3)
+
+class Opaque(OctetString):
+    TYPE = Identifier(CLASS_APPLICATION, STRUCTURE_PRIMITIVE, 4)
+
+class Counter64(Unsigned):
+    SIZE = 8
+    TYPE = Identifier(CLASS_APPLICATION, STRUCTURE_PRIMITIVE, 6)
+
+zeroDotZero = OID()
