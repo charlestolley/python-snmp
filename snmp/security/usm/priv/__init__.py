b24a00a2996880a78bb4d9a964ee3b7e93d4e89b
Charles Tolley <charlestolley@gmail.com>
2022-03-20 23:08:12 -0600

    Use cffi for Aes128Cfb; eliminate cryptography package dependency

--- snmp/security/usm/priv/__init__.py
+++ snmp/security/usm/priv/__init__.py
@@ -1,47 +1,4 @@
-from os import urandom
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from snmp.exception import IncomingMessageError
 
 class DecryptionError(IncomingMessageError):
     pass
-
-class Aes128Cfb:
-    BYTEORDER = "big"
-    KEYLEN = 16
-    SALTLEN = 8
-    SALTWRAP = 1 << (8 * SALTLEN)
-
-    def __init__(self, key):
-        if len(key) < self.KEYLEN:
-            errmsg = "key must be at least {} bytes long".format(self.KEYLEN)
-            raise ValueError(errmsg)
-
-        self.algo = algorithms.AES(key[:self.KEYLEN])
-        self.salt = int.from_bytes(urandom(self.SALTLEN), self.BYTEORDER)
-
-    def cipher(self, engineBoots, engineTime, salt):
-        if len(salt) != self.SALTLEN:
-            raise ValueError("Invalid salt")
-
-        iv = b''.join((
-            engineBoots.to_bytes(4, self.BYTEORDER),
-            engineTime .to_bytes(4, self.BYTEORDER),
-            salt
-        ))
-
-        return Cipher(
-            self.algo,
-            modes.CFB(iv),
-            default_backend()
-        )
-
-    def decrypt(self, data, *args, **kwargs):
-        decryptor = self.cipher(*args, **kwargs).decryptor()
-        return decryptor.update(data) + decryptor.finalize()
-
-    def encrypt(self, data, *args, **kwargs):
-        self.salt = (self.salt + 1) % self.SALTWRAP
-        salt = self.salt.to_bytes(self.SALTLEN, self.BYTEORDER)
-        encryptor = self.cipher(*args, salt=salt, **kwargs).encryptor()
-        return salt, encryptor.update(data) + encryptor.finalize()
---
+++ snmp/security/usm/priv/aes.py
@@ -0,0 +1,55 @@
+import os
+from snmp.openssl.aes import ffi, lib
+from . import DecryptionError
+
+class Aes128Cfb:
+    BITS = 128
+    INTSIZE = 4
+    BYTEORDER = "big"
+
+    BLOCKLEN = BITS // 8
+    KEYLEN = BLOCKLEN
+    SALTLEN = BLOCKLEN - (2 * INTSIZE)
+    SALTWRAP = 1 << (8 * SALTLEN)
+
+    def __init__(self, key):
+        if len(key) < self.KEYLEN:
+            errmsg = "key must be at least {} bytes long".format(self.KEYLEN)
+            raise ValueError(errmsg)
+
+        self.key = ffi.new("AES_KEY*")
+        result = lib.AES_set_encrypt_key(key[:self.KEYLEN], self.BITS, self.key)
+        assert result == 0
+
+        self.salt = int.from_bytes(os.urandom(self.SALTLEN), self.BYTEORDER)
+
+    def packIV(self, engineBoots, engineTime, salt):
+        if len(salt) != self.SALTLEN:
+            raise ValueError("Invalid salt")
+
+        return b''.join((
+            engineBoots.to_bytes(self.INTSIZE, self.BYTEORDER),
+            engineTime .to_bytes(self.INTSIZE, self.BYTEORDER),
+            salt
+        ))
+
+    def compute(self, data, iv, mode):
+        num = ffi.new("int*", 0)
+        iv = ffi.new("unsigned char[]", iv)
+        output = ffi.new("unsigned char[]", len(data))
+        lib.AES_cfb128_encrypt(data, output, len(data), self.key, iv, num, mode)
+        return bytes(output)
+
+    def decrypt(self, data, engineBoots, engineTime, salt):
+        try:
+            iv = self.packIV(engineBoots, engineTime, salt)
+        except ValueError as err:
+            raise DecryptionError(err) from err
+
+        return self.compute(data, iv, lib.AES_DECRYPT);
+
+    def encrypt(self, data, engineBoots, engineTime):
+        self.salt = (self.salt + 1) % self.SALTWRAP
+        salt = self.salt.to_bytes(self.SALTLEN, self.BYTEORDER)
+        iv = self.packIV(engineBoots, engineTime, salt)
+        return salt, self.compute(data, iv, lib.AES_ENCRYPT)
