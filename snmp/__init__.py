a455a341edb562bc763df6686908dc6e97566395
Charles Tolley <charlestolley@gmail.com>
2022-08-22 23:58:00 -0600

    Consolidate common objects into snmp/__init__.py

---
+++ snmp/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "Engine",
+    "SNMPv1", "SNMPv2c", "SNMPv3",
+    "noAuthNoPriv", "authNoPriv", "authPriv",
+]
+
+from snmp.engine import Engine
+from snmp.message import MessageProcessingModel
+from snmp.security.levels import *
+
+SNMPv1  = MessageProcessingModel.SNMPv1.value
+SNMPv2c = MessageProcessingModel.SNMPv2c.value
+SNMPv3  = MessageProcessingModel.SNMPv3.value
--- snmp/message/__init__.py
+++ snmp/message/__init__.py
@@ -1,5 +1,6 @@
 import enum
 
 class MessageProcessingModel(enum.IntEnum):
+    SNMPv1  = 0
     SNMPv2c = 1
-    SNMPv3 = 3
+    SNMPv3  = 3
