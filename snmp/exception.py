8c0815da8e8305fb55265a3c0224525df03dc921
Charles Tolley <charlestolley@gmail.com>
2022-11-07 12:15:31 -0700

    Document snmp.exception

--- snmp/exception.py
+++ snmp/exception.py
@@ -1,11 +1,15 @@
 class SNMPException(Exception):
+    """Base class for all run-time exceptions in this library"""
     pass
 
 class SNMPLibraryBug(AssertionError):
+    """Base class for logic errors in the code of this library"""
     pass
 
 class UnsupportedFeature(SNMPLibraryBug):
+    """A failure owing to a feature that this library does not yet support"""
     pass
 
 class IncomingMessageError(SNMPException):
+    """An error indicating a received message is invalid in some way"""
     pass
