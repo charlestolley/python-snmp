8183f9d9aa0162419954f5195399723b2d485cf6
Charles Tolley <charlestolley@gmail.com>
2022-11-11 21:45:49 -0700

    Type annotations for snmp.transport

--- snmp/transport/__init__.py
+++ snmp/transport/__init__.py
@@ -5,37 +5,47 @@ from collections import namedtuple
 import enum
 import os
 
+from snmp.typing import *
+
 TransportDomain = enum.Enum("TransportDomain", ("UDP",))
-TransportLocator = namedtuple("TransportLocator", ("domain", "address"))
 
-class TransportListener:
-    @abstractmethod
-    def hear(self, transport, address, data):
-        ...
+T = TypeVar("T")
+class TransportLocator(Generic[T]):
+    def __init__(self, domain: TransportDomain, address: T) -> None:
+        self.domain = domain
+        self.address = address
 
-class Transport:
+class Transport(Generic[T]):
+    DOMAIN: ClassVar[TransportDomain]
+
     @classmethod
-    def Locator(cls, address):
+    def Locator(cls, address: T) -> TransportLocator[T]:
         return TransportLocator(cls.DOMAIN, cls.normalizeAddress(address))
 
     @classmethod
-    def normalizeAddress(cls, address):
-        return address
-
     @abstractmethod
-    def close(self):
+    def normalizeAddress(cls, address: Any) -> T:
         ...
 
     @abstractmethod
-    def listen(self, listener):
+    def close(self) -> None:
         ...
 
     @abstractmethod
-    def send(self, address, data):
+    def listen(self, listener: "TransportListener") -> None:
         ...
 
     @abstractmethod
-    def stop(self):
+    def send(self, address: T, data: bytes) -> None:
+        ...
+
+    @abstractmethod
+    def stop(self) -> None:
+        ...
+
+class TransportListener:
+    @abstractmethod
+    def hear(self, transport: Transport[T], address: T, data: bytes) -> None:
         ...
 
 supported = ("posix")
