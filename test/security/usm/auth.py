b0035513ca62dab8480e1389268fe8eac7538e93
Charles Tolley <charlestolley@gmail.com>
2022-12-19 22:22:19 -0700

    Make security unit tests run in Python 3.6

--- test/security/usm/auth.py
+++ test/security/usm/auth.py
@@ -3,6 +3,7 @@ __all__ = [
     "HmacSha384Test", "HmacSha512Test",
 ]
 
+import re
 import unittest
 from snmp.security.usm.auth import *
 
@@ -12,13 +13,13 @@ class HmacMd5Test(unittest.TestCase):
         self.engineID = bytes(11) + b"\x02"
         self.secret = b"maplesyrup"
 
-        self.authKey = bytes.fromhex("""
+        self.authKey = bytes.fromhex(re.sub(r"\n", "", """
             52 6f 5e ed 9f cc e2 6f 89 64 c2 93 07 87 d8 2b
-        """)
+        """))
 
-        self.digest = bytes.fromhex("""
+        self.digest = bytes.fromhex(re.sub(r"\n", "", """
             07 5f 47 b1 57 95 d1 15 77 df 58 19
-        """)
+        """))
 
     def testLocalize(self):
         authKey = self.authProtocol.localize(self.secret, self.engineID)
@@ -36,14 +37,14 @@ class HmacShaTest(unittest.TestCase):
         self.engineID = bytes(11) + b"\x02"
         self.secret = b"maplesyrup"
 
-        self.authKey = bytes.fromhex("""
+        self.authKey = bytes.fromhex(re.sub(r"\n", "", """
             66 95 fe bc 92 88 e3 62 82 23 5f c7 15 1f 12 84
             97 b3 8f 3f
-        """)
+        """))
 
-        self.digest = bytes.fromhex("""
+        self.digest = bytes.fromhex(re.sub(r"\n", "", """
             60 e3 8c 0e 8d e1 8f e2 b4 17 fc 4d
-        """)
+        """))
 
     def testLocalize(self):
         authKey = self.authProtocol.localize(self.secret, self.engineID)
@@ -61,14 +62,14 @@ class HmacSha224Test(unittest.TestCase):
         self.engineID = bytes(11) + b"\x02"
         self.secret = b"maplesyrup"
 
-        self.authKey = bytes.fromhex("""
+        self.authKey = bytes.fromhex(re.sub(r"\n", "", """
             0b d8 82 7c 6e 29 f8 06 5e 08 e0 92 37 f1 77 e4
             10 f6 9b 90 e1 78 2b e6 82 07 56 74
-        """)
+        """))
 
-        self.digest = bytes.fromhex("""
+        self.digest = bytes.fromhex(re.sub(r"\n", "", """
             1d 6f 2b fe d5 dc 44 94 12 ec 42 01 72 7f d0 41
-        """)
+        """))
 
     def testLocalize(self):
         authKey = self.authProtocol.localize(self.secret, self.engineID)
@@ -86,15 +87,15 @@ class HmacSha256Test(unittest.TestCase):
         self.engineID = bytes(11) + b"\x02"
         self.secret = b"maplesyrup"
 
-        self.authKey = bytes.fromhex("""
+        self.authKey = bytes.fromhex(re.sub(r"\n", "", """
             89 82 e0 e5 49 e8 66 db 36 1a 6b 62 5d 84 cc cc
             11 16 2d 45 3e e8 ce 3a 64 45 c2 d6 77 6f 0f 8b
-        """)
+        """))
 
-        self.digest = bytes.fromhex("""
+        self.digest = bytes.fromhex(re.sub(r"\n", "", """
             42 8b f9 6e 69 98 5f f6 3e 87 1d 01 02 53 0b 44
             df 57 63 80 99 35 8f 54
-        """)
+        """))
 
     def testLocalize(self):
         authKey = self.authProtocol.localize(self.secret, self.engineID)
@@ -112,16 +113,16 @@ class HmacSha384Test(unittest.TestCase):
         self.engineID = bytes(11) + b"\x02"
         self.secret = b"maplesyrup"
 
-        self.authKey = bytes.fromhex("""
+        self.authKey = bytes.fromhex(re.sub(r"\n", "", """
             3b 29 8f 16 16 4a 11 18 42 79 d5 43 2b f1 69 e2
             d2 a4 83 07 de 02 b3 d3 f7 e2 b4 f3 6e b6 f0 45
             5a 53 68 9a 39 37 ee a0 73 19 a6 33 d2 cc ba 78
-        """)
+        """))
 
-        self.digest = bytes.fromhex("""
+        self.digest = bytes.fromhex(re.sub(r"\n", "", """
             17 c5 9b c6 90 3d e9 a7 ee bb 97 a6 6f f2 37 1b
             8d 77 2a 59 95 1f 81 96 c2 54 2a 19 75 07 b3 af
-        """)
+        """))
 
     def testLocalize(self):
         authKey = self.authProtocol.localize(self.secret, self.engineID)
@@ -139,18 +140,18 @@ class HmacSha512Test(unittest.TestCase):
         self.engineID = bytes(11) + b"\x02"
         self.secret = b"maplesyrup"
 
-        self.authKey = bytes.fromhex("""
+        self.authKey = bytes.fromhex(re.sub(r"\n", "", """
             22 a5 a3 6c ed fc c0 85 80 7a 12 8d 7b c6 c2 38
             21 67 ad 6c 0d bc 5f df f8 56 74 0f 3d 84 c0 99
             ad 1e a8 7a 8d b0 96 71 4d 97 88 bd 54 40 47 c9
             02 1e 42 29 ce 27 e4 c0 a6 92 50 ad fc ff bb 0b
-        """)
+        """))
 
-        self.digest = bytes.fromhex("""
+        self.digest = bytes.fromhex(re.sub(r"\n", "", """
             63 11 9e 45 4a 13 82 fa b6 90 e0 34 b6 3b 59 9a
             4d 5c 1a 40 c0 c6 fb 0e 2d cb 10 c7 6c 45 4e 29
             14 84 5a 89 a1 cd b7 42 4a f5 c7 07 11 c3 b9 f4
-        """)
+        """))
 
     def testLocalize(self):
         authKey = self.authProtocol.localize(self.secret, self.engineID)
--- test/security/usm/priv.py
+++ test/security/usm/priv.py
@@ -1,5 +1,6 @@
 __all__ = ["Aes128CfbTest", "DesCbcTest"]
 
+import re
 import unittest
 from snmp.ber import *
 from snmp.types import *
@@ -33,17 +34,17 @@ class Aes128CfbTest(unittest.TestCase):
         privKey = self.authProtocol.localize(self.secret, self.engineID)
         priv = self.privProtocol(privKey)
 
-        ciphertext = bytes.fromhex("""
+        ciphertext = bytes.fromhex(re.sub(r"\n", "", """
             45 80 ec 9b b4 8c 56 e2 02 68 cc d9 70 98 55 bd
             77 6f 73 75 26 1f 06 4b 62 62 9a 3b c6 e4 5d 65
             0b a7 58 49 cc 28 ea db 4b 64 c2 c2 2b 6e 04 30
             41 15 08 9b ca 45 c9 4a 58 f6 5c 5a e5 50 bb eb
             30 b4 bd 94 88 9a 3e c3 9a 89 29 39
-        """)
+        """))
 
-        msgPrivParameters = bytes.fromhex("""
+        msgPrivParameters = bytes.fromhex(re.sub(r"\n", "", """
             30 50 91 db cc 0c 9b 87 
-        """)
+        """))
 
         plaintext = priv.decrypt(
             ciphertext,
@@ -102,17 +103,17 @@ class DesCbcTest(unittest.TestCase):
         privKey = self.authProtocol.localize(self.secret, self.engineID)
         priv = self.privProtocol(privKey)
 
-        ciphertext = bytes.fromhex("""
+        ciphertext = bytes.fromhex(re.sub(r"\n", "", """
             14 4e f2 86 ad 12 47 ba 23 d6 41 51 67 17 1d 15
             aa 91 c7 fb ed 3e f2 1f 59 7f 96 ac a3 11 8d cb
             37 26 62 15 cc 32 0b 85 4a 70 91 39 82 a6 15 90
             aa 9d 0d a1 55 d2 9c 74 4f 32 2c a9 17 d9 7b 72
             2d b6 59 7d 00 c4 93 05 18 41 da 11 08 85 a8 4a 
-        """)
+        """))
 
-        msgPrivParameters = bytes.fromhex("""
+        msgPrivParameters = bytes.fromhex(re.sub(r"\n", "", """
             36 bb be 6d f8 89 34 b6 
-        """)
+        """))
 
         plaintext = priv.decrypt(
             ciphertext,
