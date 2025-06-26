2159442cdd4195a016733105284ebb6b900a3c78
Charles C. D. Tolley <charlescdtolley@protonmail.com>
2025-06-26 01:16:39 -0600

    Add Timeout, UDP_IPv4, and UDP_IPv6 to snmp/__init__.py

--- snmp/__init__.py
+++ snmp/__init__.py
@@ -1,13 +1,19 @@
 __all__ = [
-    "Engine",
+    "Engine", "Timeout",
     "SNMPv1", "SNMPv2c", "SNMPv3",
+    "UDP_IPv4", "UDP_IPv6",
     "noAuthNoPriv", "authNoPriv", "authPriv",
 ]
 
 from snmp.engine import Engine
 from snmp.message import ProtocolVersion
+from snmp.requests import Timeout
+from snmp.transport import TransportDomain
 from snmp.security.levels import *
 
 SNMPv1  = ProtocolVersion.SNMPv1
 SNMPv2c = ProtocolVersion.SNMPv2c
 SNMPv3  = ProtocolVersion.SNMPv3
+
+UDP_IPv4 = TransportDomain.UDP_IPv4
+UDP_IPv6 = TransportDomain.UDP_IPv6
