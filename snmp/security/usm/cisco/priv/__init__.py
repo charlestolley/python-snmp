34eadcc9ccc744057adffeb2315bb4f2cb5f15d6
Charles C. D. Tolley <charlescdtolley@protonmail.com>
2026-08-10 21:20:08 -0600

    Implement Cisco AES 192 and 256 algorithms

---
+++ snmp/security/usm/cisco/priv/__init__.py
@@ -0,0 +1,25 @@
+from importlib import import_module
+
+from snmp.security.usm import PrivProtocol
+from snmp.security.usm.priv import AesCfb128
+
+class DoubleLocalizePrivProtocol(PrivProtocol):
+    @classmethod
+    def localizeKey(cls, authProtocol, intermediateKey, engineID):
+        key = authProtocol.localizeKey(intermediateKey, engineID)
+
+        if len(key) < cls.KEYLEN:
+            key += authProtocol.localize(key, engineID)
+
+        return key
+
+module_path = AesCfb128.__module__.split(".")
+assert module_path[:4] == ["snmp", "security", "usm", "priv"]
+assert module_path[5] == "aes"
+
+package = module_path[4]
+aes = import_module(".aes", f"{__name__}.{package}")
+
+variables = {varname: getattr(aes, varname) for varname in aes.__all__}
+globals().update(variables)
+__all__ = list(variables)
